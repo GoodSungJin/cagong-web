d7cfecf9eb774bfee0ac6041b6524ccd02e01994
cereme(Sangyun Pae) <ceremebsy@gmail.com>
2021-07-10 23:28:00 +0900

    사진 리스트 추가

--- components/CafeDatailPage/TabView/photo.jsx
+++ components/CafeDatailPage/TabView/photo.jsx
@@ -1,7 +1,31 @@
-export default function TabViewPhoto() {
+import { css } from '@emotion/react'
+import Image from 'next/image'
+
+const containerStyle = css`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  padding: 0.5rem;
+`
+
+export default function TabViewPhoto({
+  data
+}) {
   return (
-    <div>
-      photo page
+    <div css={containerStyle}>
+      { data?.map((e, idx) => (
+        <div css={css`
+          position: relative;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          width: calc( (100% - 1.5rem) / 3);
+          margin: 0.25rem;
+          aspect-ratio: 1;
+        `} key={idx}>
+          <Image src={e} layout="fill" alt="image"/>
+        </div>
+      ))}
     </div>
   )
 }
