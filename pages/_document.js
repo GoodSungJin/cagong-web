7330a957ca761a29bee5211c769915aca55c5da0
GoodSungJin <mirepach@naver.com>
2021-07-11 00:10:59 +0900

    카카오 맵 key 수정

--- pages/_document.js
+++ pages/_document.js
@@ -7,8 +7,7 @@ export default class MyDocument extends Document {
     return (
       <Html>
 	      <Head>
-		      {/*<script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=65bae45a862fb7fc54faed710e99f1d2"></script>*/}
-		      <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=8e1c50f0b1f88741b88511273f974939"></script>
+		      <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=65bae45a862fb7fc54faed710e99f1d2"></script>
 	      </Head>
         <body>
 	        <ThemeProvider theme={theme}>
